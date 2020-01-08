ab9236abb56e00fd6c6c325104801bc2c45891fc
reyar <reyar@DESKTOP-USTQ7RH>
2020-01-07 21:14:22 -0800

    my rated page added

--- react-final/src/MyRated.js
+++ react-final/src/MyRated.js
@@ -1,11 +1,46 @@
 import React from "react";
 
-function MyRated() {
+class MyRated extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      title : ' ',
+      overview: '',
+      vote_average: '',
+      release_date : '',
+      poster : '',
+      url : `https://image.tmdb.org/t/p/w500/`};
+      const id = props.match.params.id;
+
+      this.componentDidMount();
+}
+  componentDidMount() {
+    const title = localStorage.getItem('title') ;
+    const poster = localStorage.getItem('poster') ;
+    const overview = localStorage.getItem('overview') ;
+    const release_date = localStorage.getItem('release_date') ;
+    const vote_average = localStorage.getItem('vote_average') ;
+    console.log(title)
+    this.setState( {title : title,
+                    overview: overview ,
+                    poster : poster,
+                    release_date  : release_date,
+                    vote_average : vote_average}  );
+    // console.log(item.title)
+  }
+  render(){
   return (
-    <div>
-      <h1>My Rated</h1>
+    <div className="fav-container container">
+      <h1>My Rated Movies</h1>
+      <h2>{this.state.title}</h2>
+      <img src={this.state.poster}/>
+      <p> {this.state.release_date}</p>
+      <p> {this.state.vote_average}</p>
+      <p> {this.state.overview}</p>
     </div>
   );
+  }
 }
 
 export default MyRated;
